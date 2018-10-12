840e67e518b6f2bee72c8803005e106aca55f29d
melissawarrens <melissa.warrens@bewire.be>
2018-10-12 12:38:33 +0200

    setup ionic with login

---
+++ frontend/src/pages/login/login.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {HomePage} from '../home/home';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+  loginCredentials = {
+    username: '',
+    password: ''
+  };
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  login() {
+    this.navCtrl.setRoot(HomePage);
+  }
+}
